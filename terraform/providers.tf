673ed28d466559fcee2bacff60cfff75c1abe028
Tom C.Rojo <tomascampoyrojo@gmail.com>
2025-12-23 10:00:00 +0100

    Declare Terraform and provider version constraints

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 1.7.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {
     resource_group {
